33ac8fad58c1747c953848bd86f5ce0d1cd5dccb
SkyDe <sky_de1991@outlook.com>
2023-05-25 17:39:43 +0330

    header links changed to navLinks and active css class added to header style css file

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useActions } from "../../context/ActionsContext";
 import { useShoppingCart } from "../../context/shoppingCartContext";
 
@@ -13,9 +13,9 @@ const Header = () => {
         <div className="header__content">
             <i className={`bx ${isDark ? "bx-sun" : "bx-moon"}`} onClick={handleToggleMode}></i>
             <nav className='header__nav'>
-              <Link to="/" className='header__nav--link'>Home</Link>
-              <Link to="/store" className='header__nav--link'>Store</Link>
-              <Link to="/about" className='header__nav--link'>About</Link>
+              <NavLink to="/" className='header__nav--link'>Home</NavLink>
+              <NavLink to="/store" className='header__nav--link'>Store</NavLink>
+              <NavLink to="/about" className='header__nav--link'>About</NavLink>
             </nav>
             <div onClick={() => handleToggleSideCart()} className="header__content--cart">
               <i className='bx bxs-cart-add' ></i>
